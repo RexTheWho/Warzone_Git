f983bf98f2832cb846435f67a2323a4762d5a697
RexTheWho <jrcrex1@gmail.com>
2019-08-26 22:10:52 +0800

    Bullet drop progress
    Note, using Thanatos on test3 will cause loading issues. Still needs work.

--- Warzone/hooks/TweakData.lua
+++ Warzone/hooks/TweakData.lua
@@ -1,10 +1,12 @@
 	--Projectiles
+
+	-- tweak_data.projectiles.wz_prj_50cal = deep_clone(tweak_data.projectiles.crossbow_arrow)
 	tweak_data.projectiles.wz_prj_50cal = {
 		damage = 1000,
-		launch_speed = 25,
+		launch_speed = 28000,
 		adjust_z = 0,
+		bullet_class = "InstantExplosiveBulletBase",
 		mass_look_up_modifier = 1,
-		name_id = "bm_50cal_bullet",
-		remove_on_impact = false,
+		remove_on_impact = true,
 		push_at_body_index = 0
 	}
--- Warzone/hooks/WeaponTweakData.lua
+++ Warzone/hooks/WeaponTweakData.lua
@@ -1,5 +1,5 @@
 Hooks:PostHook(WeaponTweakData, "_init_m95", "warzone_weapontweak_init", function(self, weapon_data)
 
-	self.m95.projectile_type = "wz_prj_50cal"
-	
+	self.m95.projectile_type = "crossbow_arrow"
+
 end)
