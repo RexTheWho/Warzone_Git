01260410760f4ca4b8219d892d7997ae19e46f0e
RexTheWho <jrcrex1@gmail.com>
2019-08-27 11:11:50 +0800

    Bullet drop update
    Thanatos (m95) now has bullet drop.
    Issues:
    -Bullets dont hurt targets
    -Loading issue requires level reload to use Thanatos

--- Warzone/hooks/TweakData.lua
+++ Warzone/hooks/TweakData.lua
@@ -1,12 +1,11 @@
 	--Projectiles
 
-	-- tweak_data.projectiles.wz_prj_50cal = deep_clone(tweak_data.projectiles.crossbow_arrow)
+	tweak_data.projectiles.wz_prj_50cal = deep_clone(tweak_data.projectiles.crossbow_arrow)
 	tweak_data.projectiles.wz_prj_50cal = {
 		damage = 1000,
 		launch_speed = 28000,
 		adjust_z = 0,
-		bullet_class = "InstantExplosiveBulletBase",
 		mass_look_up_modifier = 1,
-		remove_on_impact = true,
-		push_at_body_index = 0
+		push_at_body_index = ,
+		remove_on_impact = true
 	}
