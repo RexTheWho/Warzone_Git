f651937ccab989222602d95ca0fd452078faf9a2
RexTheWho <jrcrex1@gmail.com>
2019-08-28 05:49:32 +0800

    Asset work

--- Warzone/hooks/ProjectilesTweakData.lua
+++ Warzone/hooks/ProjectilesTweakData.lua
@@ -2,8 +2,8 @@ Hooks:PostHook(BlackMarketTweakData, "_init_projectiles", "wz_projectilestweak_i
 
 	self.projectiles.wz_prj_50cal = deep_clone(self.projectiles.crossbow_arrow)
 	self.projectiles.wz_prj_50cal = {
-		unit = "units/pd2_mod_warzone/weapons/wpn_prj_50cal/wpn_prj_50cal",
-		local_unit = "units/pd2_mod_warzone/weapons/wpn_prj_50cal/wpn_prj_50cal_local",
+		unit = "units/pd2_mod_warzone/weapons/wz_prj_50cal/wz_prj_50cal",
+		local_unit = "units/pd2_mod_warzone/weapons/wz_prj_50cal/wz_prj_50cal_local",
 		weapon_id = "m95",
 		no_cheat_count = true,
 		client_authoritative = true,
