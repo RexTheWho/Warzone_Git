7e335c30b90f316db71c0428d0f614d74d5651ed
RexTheWho <jrcrex1@gmail.com>
2019-08-29 09:52:46 +0800

    Quick update

--- Warzone/hooks/charactertweakdata.lua
+++ Warzone/hooks/charactertweakdata.lua
@@ -1,30 +1,19 @@
--- Hooks:PostHook(CharacterTweakData,"init","warzone_charactertweak_init",function(self)
+Hooks:PostHook(CharacterTweakData,"init","warzone_charactertweak_init",function(self)
 
-	-- local presets = self:_presets(tweak_data)
+	self:_init_region_terror(presets)
 
-	-- self.wz_zeal_light = deep_clone(presets.base)
-	-- self.wz_zeal_light.experience = {}
-	-- self.wz_zeal_light.weapon = presets.weapon.normal
-	-- self.wz_zeal_light.detection = presets.detection.normal
-	-- self.wz_zeal_light.HEALTH_INIT = 8
-	-- self.wz_zeal_light.headshot_dmg_mul = 2
-	-- self.wz_zeal_light.move_speed = presets.move_speed.fast
-	-- self.wz_zeal_light.surrender_break_time = {
-		-- 6,
-		-- 10
-	-- }
-	-- self.wz_zeal_light.suppression = presets.suppression.no_supress
-	-- self.wz_zeal_light.surrender = false
-	-- self.wz_zeal_light.weapon_voice = "1"
-	-- self.wz_zeal_light.experience.cable_tie = "tie_swat"
-	-- self.wz_zeal_light.speech_prefix_p1 = "rb2"
-	-- self.wz_zeal_light.access = "teamAI4"
-	-- self.wz_zeal_light.dodge = presets.dodge.athletic
-	-- self.wz_zeal_light.no_arrest = true
-	-- self.wz_zeal_light.chatter = presets.enemy_chatter.no_chatter
-	-- self.wz_zeal_light.melee_weapon = "fists"
-	-- self.wz_zeal_light.melee_weapon_dmg_multiplier = 3
-	-- self.wz_zeal_light.steal_loot = false
-	-- self.wz_zeal_light.rescue_hostages = false
+end)
 
--- end)
+
+function CharacterTweakData:_init_region_terror()
+	self._default_chatter = "dsp_radio_russian"
+	self._unit_prefixes = {
+		cop = "r",
+		swat = "r",
+		heavy_swat = "r",
+		taser = "rtsr",
+		cloaker = "rclk",
+		bulldozer = "rbdz",
+		medic = "rmdc"
+	}
+end
